fb53341ddc6dd554e9343e7e5272d687cafc649f
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-01-24 00:03:03 +0530

    Created sidebar and done with basic routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter ,Routes,Route } from 'react-router-dom';
+import ChatLayout from './layout/chatLayout';
+import Chat from './components/chat';
 
 function App() {
   return (
-    <h1>welcome To GeminiCraft</h1>
+  <>
+    <BrowserRouter>
+        <div>
+        <Routes>          
+          <Route path="/" element={<ChatLayout />} >
+            <Route path="/" element={< Chat/>} />
+            <Route path="/conversation/:id" element={< Chat/>} />
+          </Route>
+        </Routes>
+        </div>
+    </BrowserRouter>
+  </>
   );
 }
 
