3ced45f15416a06c485c03814c109498d5a425bc
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-03-02 16:40:04 +0530

    resolved small issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatLayout from "./layout/ChatLayout";
-import { ThemeProvider } from "./store/ThemeContext";
+import { ThemeProvider } from "./store/ThemeContext"; // Ensure correct casing
 import Profile from "./components/Profile";
 import { AuthProvider } from "./store/AuthContext";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
-import { ChatProvider } from "./store/ChatContext"; 
+import { ChatProvider } from "./store/ChatContext"; // Ensure correct casing
 import Chat from "./components/Chat";
 
 function App() {
