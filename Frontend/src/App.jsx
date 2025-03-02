a8d33b4b5317b10af01b2627c39f5c5ac1f52531
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-03-02 15:21:30 +0530

    updated the casing of the file imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter ,Routes,Route } from 'react-router-dom';
-import ChatLayout from './layout/ChatLayout';
-import Chat from './components/chat';
-import { ChatProvider } from './store/chatContext';
-import { ThemeProvider } from './store/themeContext';
-import Profile from './components/Profile';
-import { AuthProvider } from './store/authContext';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChatLayout from "./layout/ChatLayout";
+import { ThemeProvider } from "./store/ThemeContext";
+import Profile from "./components/Profile";
+import { AuthProvider } from "./store/AuthContext";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { ChatProvider } from "./store/ChatContext"; 
+import Chat from "./components/Chat";
 
 function App() {
   console.log("backend url", import.meta.env.VITE_BACKEND_URL);
