32b9d0193cd248a8d32b3727a8fe631f7630fa59
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-03-27 12:44:42 +0530

    added openAI api key to .env

--- Backend/service/openAIService.js
+++ Backend/service/openAIService.js
@@ -2,8 +2,7 @@ const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey:
-    "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",
+  apiKey:process.env.OPENAI_APIKEY,
 });
 
 const newAPI = async (prompt) => {
