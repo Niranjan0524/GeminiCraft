c7244154c1e02389de780d1b55ad150b4f5ee854
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-07-04 21:19:53 +0530

    updated gitignore

--- Backend/service/openAIService.js
+++ Backend/service/openAIService.js
@@ -2,7 +2,7 @@ const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey:process.env.OPENAI_APIKEY,
+  apiKey: process.env.OPENAI_APIKEY,
 });
 
 const newAPI = async (prompt) => {
